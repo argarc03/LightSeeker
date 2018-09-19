ff5296339790d33e58ce94f862923708316f2a51
argarc03 <argarc03@ucm.es>
2018-09-19 12:58:59 +0200

    Cambiado screen size, cambiado background-interfaz y añadido sprite de Spider

--- src/js/main.js
+++ src/js/main.js
@@ -20,18 +20,30 @@ var BootScene = {
 
 var PreloaderScene = {
   preload: function () {
+    // scale the game 4x
+    this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    this.game.scale.setUserScale(4, 4);
+
+    // enable crisp rendering
+    this.game.renderer.renderSession.roundPixels = true;
+    Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+
+
     this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
     this.loadingBar.anchor.setTo(0, 0.5);
     this.load.setPreloadSprite(this.loadingBar);
 
     // TODO: load here the assets for the game
     //images
+    
     this.game.load.image('logo', 'images/phaser.png');
     this.game.load.image('imagenmenu', 'assets/images/Village.jpg');
     this.game.load.image('seeker', 'assets/images/seeker.png');
     this.game.load.image('enemy', 'assets/images/enemy.png');
     this.game.load.image('combatbackground', 'assets/images/combatbackground.png');
     this.game.load.image('eventimage', 'assets/images/eventimage.jpg');
+
+    this.game.load.image('spider', 'assets/images/spider.png');
     //fonts
     this.game.load.bitmapFont('font', 'assets/fonts/bitmapFonts/font.png', 
     'assets/fonts/bitmapFonts/font.fnt');
@@ -41,13 +53,14 @@ var PreloaderScene = {
   },
 
   create: function () {
-    this.game.state.start('event');
+    this.game.state.start('combat');
   }
 };
 
 window.onload = function () {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
-
+  var game = new Phaser.Game(200, 150, Phaser.AUTO, 'game',
+    
+  );
   game.state.add('boot', BootScene);
   game.state.add('preloader', PreloaderScene);
   game.state.add('mainmenu', MainMenuScene);
