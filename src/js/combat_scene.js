8f8d359833a8055cd7ae97df0e485a9cbc2f8f95
Carlos Durán Domínguez <carduran@ucm.es>
2018-09-28 17:48:12 +0200

    Actualización clase personaje

--- src/js/combat_scene.js
+++ src/js/combat_scene.js
@@ -13,24 +13,24 @@ var CombatScene = {
 
   create: function () {
     //render background
-    var combatbackground = this.game.add.sprite(0,0, 'combatbackground');
+    var combatbackground = this.game.add.sprite(0, 0, 'combatbackground');
 
-    var action = [];
-    seeker.actions = {};
-    seeker.actions.idle = function() {};
-    seeker.actions.attack = function() {};
-    action[1] = new Action('idle', 5, true,[0,1,2,3,4,5,6,7,8], 10, function(object){});
-    action[0] = new Action('attack', 5, false,[17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34], 10, function(object){}, seeker.actions.idle);
-    
-    seeker = new Character(-8,0,'seeker', 1, 1, 1, 100, 'seekerAnimations', this.game, action);
-    seeker.actions.attack();
+    var fun = function(args){
+      console.log(args);
+    }
+    var fun = function(){
+      for(arg in arguments)
+        console.log(arg);
+    }
+
+    fun('hola', 'adios', 'muy buenas');
     //music
     var music = this.game.add.audio('boss');
     music.play();
 
-    
+
   },
-  update: function() { 
+  update: function () {
   }
 };
 
