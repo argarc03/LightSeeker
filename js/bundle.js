a8a3efcf295d11dba85266434015ea6cec1bbb72
argarc03 <argarc03@ucm.es>
2018-12-11 05:24:39 +0100

    Update 2018-12-11T04:24:35.745Z

--- js/bundle.js
+++ js/bundle.js
@@ -3699,6 +3699,7 @@ var PreloaderScene = {
   create: function () {
       this.game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
       PreloaderScene.game.state.start('intro');
+      
   }
 
 };
