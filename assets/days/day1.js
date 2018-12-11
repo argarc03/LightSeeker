3a9ab3128258c179b4c84c6dc38c075464697e5c
argarc03 <argarc03@ucm.es>
2018-12-11 05:22:09 +0100

    Update 2018-12-11T04:22:07.206Z

---
+++ assets/days/day1.js
@@ -0,0 +1,17 @@
+var DayFunctions = require('../../js/manager/dayFunctions');
+
+var Day0 = {
+    DayGenerator: function (seeker, dayManager) {
+        return 'Luces';
+    },
+    Luces: function (seeker, dayManager) {
+        return {
+            text: 'Esto es un texto de prueba2',
+            image: 'eventImageError',
+            options: [{ text: 'Seguir', callback: DayFunctions.NextDay, arguments: [] }],
+            music: 'intro'
+        }
+    }
+}
+
+module.exports = Day0;
